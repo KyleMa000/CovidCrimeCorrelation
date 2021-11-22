4db1c2a81fdaf7eb5cb4577f0d65e2655e1631f9
Kyle <hm1920@nyu.edu>
2021-11-22 10:13:52 -0500

    adding code and readme format

--- profiling_code/kyle/CountRecsMapper.java
+++ profiling_code/kyle/CountRecsMapper.java
@@ -7,7 +7,7 @@ import org.apache.hadoop.mapreduce.Mapper;
 
 
 
-public class CleanMapper
+public class CountRecsMapper
 	extends Mapper<LongWritable, Text, Text, IntWritable>{
 
 
@@ -15,27 +15,18 @@ public class CleanMapper
 	public void map(LongWritable key, Text value, Context context)
 		throws IOException, InterruptedException{
 
-
-		String[] line = value.toString().split(",");
-
-
-		if (line[1].equals("US_NY") | line[1].equals("US_NY_NYC")){
-
-			for(int i = 0; i < line.length; i++){
-				if(line[i].isEmpty()){
-					line[i] = "0";
-				}
-
-
-
-			}
-			String out = String.join(",", line);
-			context.write( new Text(out), new IntWritable(1));
-		}
-		else{
+		if (key.get() == 0){
 			return;
 		}
- 
+
+		String temp = "Total number of records: ";
+
+
+
+
+		context.write(new Text(temp), new IntWritable(1));
+
+
 
 	}
 }
